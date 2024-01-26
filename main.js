48a72906cec3dd0c7c13810825cd9d37002c09c1
Raxylle <127960669+Raxylle@users.noreply.github.com>
2024-01-27 02:10:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesbtn = document.querySelector(".yes-btn");
+const nobtn = document.querySelector(".no-btn");
+
+yesbtn.addEventListener("click", () => {
+    question.innerHTML = "Yay, See you ";
+    gif.src = "E:/Downloads/yey.gif";
+
+    yesbtn.addEventListener("click", () => {
+        question.innerHTML = "Thanks LOVE";
+        gif.src = "E:/Downloads/thank you.gif";
+    });    
+});
+
+nobtn.addEventListener("mouseover", () => {
+    const noBtnRect = nobtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    nobtn.style.left = randomX + "px";
+    nobtn.style.top = randomY + "px"; 
+});
